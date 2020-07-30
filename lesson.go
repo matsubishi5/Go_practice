a8434374b2148800fcad28df9b52e04801916d05
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-07-30 12:32:32 +0900

    [learn]variable

--- lesson.go
+++ lesson.go
@@ -1,10 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"time"
+import "fmt"
+
+var (
+	i int = 1
+	f64 float64 = 1.2
+	s string = "test"
+	t, f bool = true, false
 )
 
 func main() {
-	fmt.Println("Hello World", time.Now())
+	fmt.Println(i, f64, s, t, f)
+
+	xi := 1
+	xi = 2
+	var xf32 float32 = 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Printf("%T\n", xf32)
+	fmt.Println(xi, xf32, xs, xt, xf)
 }
