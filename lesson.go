b6e0f30127748b340da0ea6d0940ec0eb6cb1ca2
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-07-30 13:04:36 +0900

    [learn]string

--- lesson.go
+++ lesson.go
@@ -1,35 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	//var (
-	//	u8 uint8 = 255
-	//	i8 int8 = 127
-	//	f32 float32 = 0.2
-	//	c64 complex64 = -5 + 12i
-	//)
-	//fmt.Println(u8, i8, f32, c64)
-	//fmt.Printf("type=%T value=%v", u8, u8)
+	fmt.Println("Hello World")
+	fmt.Println("Hello " + "World")
+	fmt.Println(string("Hello World"[0]))
 
-	//fmt.Println("1 + 1 = ", 1+1)
-	//fmt.Println("2 = 1 = ", 2-1)
-	//fmt.Println("2 * 2 = ", 2*2)
-	//fmt.Println("4 / 2 = ", 4/2)
-	//fmt.Println("10 / 3 = ", 10/3)
-	//fmt.Println("10.0 / 3 = ", 10.0/3)
-	//fmt.Println("10 / 3.0 = ", 10/3.0)
-	//fmt.Println("10 % 3 = ", 10%3)
-
-	//x := 0
-	//fmt.Println(x)
-	//x++
-	//fmt.Println(x)
-	//x--
-	//fmt.Println(x)
-
-	fmt.Println(1 << 0)
-	fmt.Println(1 << 1)
-	fmt.Println(1 << 2)
-	fmt.Println(1 << 3)
+	var s string = "Hello World"
+	s = strings.Replace(s, "H", "X", 1)
+	fmt.Println(s)
+	fmt.Println(strings.Contains(s, "World"))
 }
